9b6d88a7cee5fcdb37643c16122d46f530d6d3ea
apple <>
2019-01-24 10:27:22 +0800

    http

---
+++ WebServer/Sources/WebServer/Routes.swift
@@ -0,0 +1,24 @@
+//
+//  Routes.swift
+//  COpenSSL
+//
+//  Created by apple on 2019/1/23.
+//
+
+import PerfectLib
+import PerfectHTTP
+import PerfectSession
+
+public func makeRoutes() -> Routes {
+    var routes = Routes()
+    routes.add(uri: "/") { (request:HTTPRequest, _ response:HTTPResponse) in
+        var opt = [String: Any]()
+        opt["sessionid"] = request.session?.token
+        opt["dump"] = request.session?.data
+        opt["key"]  = "zhuzhenhua"
+        
+        _ = try? response.setBody(json: opt)
+        response.completed()
+    }
+    return routes
+}
--- WebServer/Sources/WebServer/main.swift
+++ WebServer/Sources/WebServer/main.swift
@@ -1,1 +1,12 @@
-print("Hello, world!")
+import PerfectLib
+import PerfectHTTP
+import PerfectHTTPServer
+
+let server = HTTPServer()
+server.serverPort = 8181;
+server.addRoutes(makeRoutes())
+do {
+    try server.start()
+} catch PerfectError.networkError(let err, let msg)  {
+    
+}
